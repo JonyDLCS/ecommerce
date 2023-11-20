78163cb275b17f7d124f093d9d9a4cc3d31a857d
Jonathan De La Cruz <jony_ed@hotmail.com>
2023-11-20 00:47:39 -0700

    navbar progress

--- app/page.js
+++ app/page.js
@@ -1,10 +1,20 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import snsLogo from '@/public/snsLogo.svg';
+import './globals.css';
+import { SnsLogo } from '@/public/snsLogo';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-
+    <main className="bg-slate-00">
+      <div className=" pt-8 text-center flex flex-col justify-center items-center gap-4">
+        <ul className=' flex flex-row gap-16 font-semibold list-none items-center'>
+          <li className=' cursor-pointer hover:text-fuchsia-700' ><p>Tienda</p></li>
+          <li className=' cursor-pointer '>
+            <SnsLogo />
+          </li>
+          <li className=' cursor-pointer hover:text-fuchsia-700'><p>Acerca De</p></li>
+          
+        </ul>
       </div>
     </main>
   )
